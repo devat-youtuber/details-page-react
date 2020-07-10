4d18ec15cef9c232d42890981ac9acfbefcdab05
dev at <battuosai01@gmail.com>
2020-07-10 21:36:32 +0700

    Updates

---
+++ precache-manifest.f63bd5e9d714bb2bf2d6cf3cf1b8f817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc34851d81752294eed9976118cbe83b",
+    "url": "/details-page-react/index.html"
+  },
+  {
+    "revision": "867df6907926f646644b",
+    "url": "/details-page-react/static/css/main.603cb7c2.chunk.css"
+  },
+  {
+    "revision": "b3407a034bd5709b59eb",
+    "url": "/details-page-react/static/js/2.5baeb868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/details-page-react/static/js/2.5baeb868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867df6907926f646644b",
+    "url": "/details-page-react/static/js/main.38f7b188.chunk.js"
+  },
+  {
+    "revision": "0d599af67818be60d6df",
+    "url": "/details-page-react/static/js/runtime-main.f491c5d6.js"
+  }
+]);
